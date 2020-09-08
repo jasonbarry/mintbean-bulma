13f1cdc375e6e03846bd828bb59e80810f440373
jasonbarry <jasbarry@me.com>
2020-09-08 15:23:03 -0700

    New stuff yep yep

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           <code>App.js</code> is the entrypoint to your app.
         </p>
-        <h2>HAPPY HACKING!</h2>
+        <h2>New stuff yep yep</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
